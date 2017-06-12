ffb8a092994099c13a5a9c90c8dafd5ee3dda0e2
Jacob Kato <jacobkato49@yahoo.com>
2017-06-11 17:23:45 -0700

    everything fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
  
-
-
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBJIvQZ1ZZPfsOvCmM_uA786V0J2QyET8A",
@@ -19,20 +17,21 @@ var database= firebase.database()
 
 
 
+
 // CAPTURE BUTTON CLICK
 $("#submit").on("click", function() {
+	event.preventDefault();
 
-	//definitons
-
+	//DEFINITION
 		//.val()- method that returns or sets the value attribute of the selected form element
-
 		//.trim()- method that removes the whitespace from the beginning and end of the string 
 
 	//Values for each variable and place them correctly in the html
-	var name= ("#nameInput").val().trim();
-	var dest= ("#destInput").val().trim();
-	var time= ("#timeInput").val().trim();
-	var freq= ("#freqInput").val().trim();
+	var name= $("#nameInput").val().trim();
+	var dest= $("#destInput").val().trim();
+	var time= $("#timeInput").val().trim();
+	var freq= $("#freqInput").val().trim();
+
 
 	//Push the variables and reference them to the database location
 	database.ref().push({
@@ -41,12 +40,10 @@ $("#submit").on("click", function() {
 		name: name,
 		destination: dest,
 		time: time,
-		frequence: freq,
-		timeAdded: firebase.database.ServerValue.TIMESTAMP  //Ask the TA's why we do this
+		frequency: freq,
+		timeStamp: firebase.database.ServerValue.TIMESTAMP  //Ask the TA's why we do this
 
-		//
 	});
-
 
 	//No refresh
 	$("input").val("");
@@ -54,16 +51,80 @@ $("#submit").on("click", function() {
 });
 
 
-// On-click Child Functions
+// On-click Child Function
+database.ref().on("child_added", function(childSnapshot){ 
+	//childSnapshot returns an object
+	console.log("The childSnapshot Data", childSnapshot.val());
 
 
-// Convert Train Times
+	var name = childSnapshot.val().name;
+	var dest = childSnapshot.val().destination;
+	var time = childSnapshot.val().time;
+	var freq = childSnapshot.val().frequency;
+
+	// DEFINITION- 
+		//child_added event is typically used when retrieving a list of items from the database
+
+	// DEBUG HERE
+	// console.log("name: " + name);
+	// console.log("time: " + time);
+
+
+
+	//***Convert TRAIN TIMES***//
+
+		// DEFINITION-
+			//The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+
+	var freq = parseInt(freq); //making sure the frequency is a numerical value
+
+	//Current Time
+	var currentTime= moment(); //this obtains current date and time from MOMENT JS
+
+	//FIRST TIME: PUSHED BACK ONE YEAR TO COME BEFORE CURRENT TIME
+	var dConverted = moment(childSnapshot.val().time, 'HH:mm').subtract(1, 'years'); //military time notation
+	console.log("DATE CONVERTED: " + dConverted);
+
+	var trainTime = moment(dConverted).format('HH:mm');
+	console.log("TRAIN TIME : " + trainTime);
+	
+	//DIFFERENCE B/T THE TIMES 
+	var tConverted = moment(trainTime, 'HH:mm').subtract(1, 'years');
+	var tDifference = moment().diff(moment(tConverted), 'minutes');
+	console.log("DIFFERENCE IN TIME: " + tDifference);
+
+	//REMAINDER 
+	var tRemainder = tDifference % freq;
+	console.log("TIME REMAINING: " + tRemainder);
+
+	//MINUTES UNTIL NEXT TRAIN
+	var minsAway = freq - tRemainder;
+	console.log("MINUTES UNTIL NEXT TRAIN: " + minsAway);
+
+	//NEXT TRAIN
+	var nextTrain = moment().add(minsAway, 'minutes');
+	console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm A'));
 
 
 
 // Table Data
+$("#currentTime").text(currentTime);
+$("#trainTable").append(
+		"<tr><td id='nameDisplay'>" + childSnapshot.val().name +
+		"</td><td id='destDisplay'>" + childSnapshot.val().destination +
+		"</td><td id='freqDisplay'>" + childSnapshot.val().frequency +
+		"</td><td id='nextDisplay'>" + moment(nextTrain).format("HH:mm") +
+		"</td><td id='awayDisplay'>" + minsAway  + ' minutes until arrival' + "</td></tr>");
 
+
+},
 
 // errorObject
+function errorObject () {
+    console.log("Read failed: " + errorObject.code)
+
+});
+
+
 
 
